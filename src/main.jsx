43545a272dbe8d15243bebfdbddf1aba1062fb5e
nithishp <nithishj03@gmail.com>
2024-01-31 19:47:39 +0530

    adding tailwind components

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import RegisterScreen from './pages/RegisterScreen.jsx'
+import LoginScreen from './pages/LoginScreen.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/register' element={<RegisterScreen/>}/>
+      <Route path='/login' element={<LoginScreen/>}/>
+    </Routes>
+  </BrowserRouter>
+    
+  
 )
