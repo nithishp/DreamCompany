d6c559b245da2c463c4e393f114cb7a1a85817db
nithishp <nithishj03@gmail.com>
2025-01-30 17:38:01 +0530

    Button FIX link

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({label,iconURL}) => {
   function gotologin(){
-      window.location.href = 'https://docs.google.com/forms/d/e/1FAIpQLSfTphfiPqv_M_5NA2xbU8vcJZ02-8cWuZpef9rw448Dq20zaA/viewform'
+      window.location.href = '/#contactus'
   }
   return (
     <button onClick={gotologin} className='flex justify-center items-center leading-none border bg-red-600 rounded-full text-white px-7 py-4 font-montserrat text-lg gap-2 '>
