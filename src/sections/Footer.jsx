1c73f584a07c76a2e5f9aa94ed16bea636ffac9b
nithishp <nithishj03@gmail.com>
2025-01-30 18:34:15 +0530

    ADDED: Footer links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import favicon from "../persnalAsset/logo-wbg.png";
-import { socialMedia } from "../constants";
+import { navLinks, socialMedia } from "../constants";
 
 const Footer = () => {
   return (
@@ -8,11 +8,31 @@ const Footer = () => {
       <div className="flex flex-wrap items-start justify-between gap-20 max-lg:flex-col">
         <div className="flex flex-col items-center">
           <a href="/">
-            <img src={favicon} height={70} width={70} className="size-32 object-cover" />
+            <img
+              src={favicon}
+              height={70}
+              width={70}
+              className="size-32 object-cover"
+            />
           </a>
+          <ul className="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="text-white transition text-xl hover:text-white/75"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+
+       
+          </ul>
           <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
             &copy; DreamCompany 2024. All rights reserved.
           </p>
+
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
               <div
